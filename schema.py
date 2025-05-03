2cc028e27218c1d0a75322f08972474743858b5e
Philippe Marchand <PHMAR122@ulaval.ca>
2025-05-03 17:42:50 -0400

    save 6

--- schema.py
+++ schema.py
@@ -1,26 +1,40 @@
 import numpy as np
-from euler_explicite import euler_explicite
+
 
 def schema(sigma, L, f, h, tau, K):
-  
+    """
+    Schéma en différences finies + Euler explicite « à la main »
+    pour u_t = σ u_xx sur [0,L], u(0)=u(L)=0.
+
+    Renvoie W de forme (n-1, K+1) avec W[:,k] = u^k aux points intérieurs.
+    """
+    # nombre de sous-intervalles spatiaux
     n = int(round(L / h))
 
-    factor = sigma / h**2
-    A_h = np.zeros((n-1, n-1))
-    for i in range(n-1):
-        A_h[i, i] = -2 * factor
-        if i > 0:
-            A_h[i, i-1] = factor
-        if i < n-2:
-            A_h[i, i+1] = factor
-
-    def F(t, U):
-        return A_h @ U
-
+    # points intérieurs x_j = j*h, j=1…n-1
     x_int = np.linspace(h, L-h, n-1)
-    U0 = f(x_int)
+    U = f(x_int)               # U^0
+    W = np.zeros((n-1, K+1))
+    W[:, 0] = U.copy()
 
-    tf = K * tau
-    t_vals, W = euler_explicite(F, t0=0.0, y0=U0, h=tau, tf=tf)
+    # coefficient de diffusion numérique
+    mu = sigma * tau / h**2
+
+    for k in range(K):
+        U_new = np.zeros_like(U)
+
+        # j = 1 (U[0]) avec u(0)=0
+        U_new[0] = U[0] + mu*(U[1] - 2*U[0] + 0)
+
+        # j = 2…n-2 (U[1]…U[n-3])
+        for j in range(1, n-2):
+            U_new[j] = U[j] + mu*(U[j+1] - 2*U[j] + U[j-1])
+
+        # j = n-1 (U[n-2]) avec u(L)=0
+        U_new[-1] = U[-1] + mu*(0 - 2*U[-1] + U[-2])
+
+        # sauvegarde et avance
+        W[:, k+1] = U_new
+        U = U_new
 
     return W
